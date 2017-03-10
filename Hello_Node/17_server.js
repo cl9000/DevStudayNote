09b6a14be17366b5e4f5d1ea359bf8c266a24b9b
weichanglu <cl9000@126.com>
2017-03-10 11:18:35 +0800

    commit node

---
+++ Hello_Node/17_server.js
@@ -0,0 +1,71 @@
+/**
+ * Created by weichanglu on 2017/3/10.
+ */
+
+/** ****************************Get************************************/
+//Get
+//var express = require('express');
+//var app = express();
+//
+////app.use(express.static('public'));
+//
+//app.get('/17_index.html', function (req, res) {
+//    res.sendFile( __dirname + "/" + "17_index.html" );
+//})
+//
+//app.get('/process_get', function (req, res) {
+//
+//    // 输出 JSON 格式
+//    response = {
+//        first_name:req.query.first_name,
+//        last_name:req.query.last_name
+//    };
+//    console.log(response);
+//    res.end(JSON.stringify(response));
+//})
+//var server = app.listen(8080, function () {
+//
+//    var host = server.address().address
+//    var port = server.address().port
+//
+//    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+//
+//})
+/** ****************************Get************************************/
+
+
+/** ****************************Post************************************/
+//POST
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//app.use(express.static('public'));
+
+app.get('/17_index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "17_index.html" );
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+    // 输出 JSON 格式
+    response = {
+        first_name:req.body.first_name,
+        last_name:req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8080, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
+/** ****************************Post************************************/
