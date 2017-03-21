c6446513e71bb47823ac72d078a75f112eb31bf7
weichanglu <cl9000@126.com>
2017-03-21 14:47:23 +0800

    commit Hello_html change Hello_Node

--- Hello_Node/17_server.js
+++ Hello_Node/17_server.js
@@ -1,7 +1,12 @@
 /**
  * Created by weichanglu on 2017/3/10.
  */
-
+/**
+//$ npm install express --save
+//$ npm install body-parser --save
+//$ npm install cookie-parser --save
+//$ npm install multer --save
+ */
 /** ****************************Get************************************/
 //Get
 //var express = require('express');
@@ -36,28 +41,75 @@
 
 /** ****************************Post************************************/
 //POST
+//var express = require('express');
+//var app = express();
+//var bodyParser = require('body-parser');
+//
+//// 创建 application/x-www-form-urlencoded 编码解析
+//var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//
+////app.use(express.static('public'));
+//
+//app.get('/17_index.html', function (req, res) {
+//    res.sendFile( __dirname + "/" + "17_index.html" );
+//})
+//
+//app.post('/process_post', urlencodedParser, function (req, res) {
+//
+//    // 输出 JSON 格式
+//    response = {
+//        first_name:req.body.first_name,
+//        last_name:req.body.last_name
+//    };
+//    console.log(response);
+//    res.end(JSON.stringify(response));
+//})
+//
+//var server = app.listen(8080, function () {
+//
+//    var host = server.address().address
+//    var port = server.address().port
+//
+//    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+//
+//})
+/** ****************************Post************************************/
+
+/** ****************************文件上传************************************/
 var express = require('express');
 var app = express();
+var fs = require("fs");
+
 var bodyParser = require('body-parser');
+var multer  = require('multer');
 
-// 创建 application/x-www-form-urlencoded 编码解析
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-//app.use(express.static('public'));
+app.use(express.static('17_public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
 
 app.get('/17_index.html', function (req, res) {
     res.sendFile( __dirname + "/" + "17_index.html" );
 })
 
-app.post('/process_post', urlencodedParser, function (req, res) {
+app.post('/file_upload', function (req, res) {
 
-    // 输出 JSON 格式
-    response = {
-        first_name:req.body.first_name,
-        last_name:req.body.last_name
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = __dirname + "/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if( err ){
+                console.log( err );
+            }else{
+                response = {
+                    message:'File uploaded successfully',
+                    filename:req.files[0].originalname
+                };
+            }
+            console.log( response );
+            res.end( JSON.stringify( response ) );
+        });
+    });
 })
 
 var server = app.listen(8080, function () {
@@ -68,4 +120,4 @@ var server = app.listen(8080, function () {
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
 })
-/** ****************************Post************************************/
+/** ****************************文件上传************************************/
